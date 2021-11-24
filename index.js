ee25a084d88155c1142c14e41b7430f506dcbab8
Spencer Knight <spencer.knightusn@gmail.com>
2021-11-24 16:49:23 -0700

    aside boilerplate complete

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 (function headerListener() {
   const NAV = Array.from(document.getElementsByClassName('nav-bar'))[0];
-  console.log(NAV);
   function navHandler(event) {
     if (window.pageYOffset >= 89) {
       NAV.classList.add('over');
